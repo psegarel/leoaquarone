78e76e783fee6fc7746d4d34ff731b4efbccdc50
psegarel <contact@patrick-segarel.com>
2012-10-13 14:22:57 +0800

    New version design

--- sites/all/themes/pactum_bootstrap/templates/page.tpl.php
+++ sites/all/themes/pactum_bootstrap/templates/page.tpl.php
@@ -1,18 +1,66 @@
+<header id="navbar" role="banner" class="navbar navbar-fixed-top">
+  <div class="navbar-inner">
+  	<div class="container">
+  	  <!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+  	  <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+  		<span class="icon-bar"></span>
+  		<span class="icon-bar"></span>
+  		<span class="icon-bar"></span>
+  	  </a>
+  	  
+  	  <?php if ($logo): ?>
+    		<a class="brand" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+    		  <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+    		</a>
+  	  <?php endif; ?>
+
+  	  <?php if ($site_name || $site_slogan): ?>
+    		<hgroup id="site-name-slogan">
+    		  <?php if ($site_name): ?>
+    			<h1>
+    			  <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="brand"><?php print $site_name; ?></a>
+    			</h1>
+    		  <?php endif; ?>
+    		</hgroup>
+  	  <?php endif; ?>
+  	  
+  	  <div class="nav-collapse">
+    	  <nav role="navigation">
+			<ul class="nav">
+				<li><a href="/"><i class="icon-home icon-black"></i> Home</a></li>
+				<li class=""><a href="/disclaimer">Disclaimer</a></li>
+				<li><a class="external link" href="http://www.pillar3.eu/pam1200203/">Public Compliance Disclosures</a></li>
+				<li><a class="external link" href="mailto:leonard.aquarone@pactumam.com"><i class="icon-envelope icon-black"></i> Contact Us</a></li>
+			</ul>
+			
+      		<?php if ($primary_nav): ?>
+      		  <?php /* print $primary_nav; */ ?>
+      		<?php endif; ?>
+      	  
+      		<?php if ($search): ?>
+      		  <?php /* if ($search): print render($search); endif; */ ?>
+      		<?php endif; ?>
+      		
+      		<?php if ($secondary_nav): ?>
+      		  <?php /* print $secondary_nav; */ ?>
+      		<?php endif; ?>
+    		</nav>
+  	  </div>         
+  	</div>
+  </div>
+</header>
 <section id="header">
-	<header id="navbar" role="banner" class="navbar navbar-fixed-top">
-		<div class="container">
-			<!-- Header -->
-			<div class="contact-info span12">
-				<a href="/"><img src="/sites/default/files/images/PAMlogo.jpg" /></a>
-				<p>Manager of the Globersel Pactum Natural Resources Fund</p>
-			</div>
+	<div class="container">
+		<!-- Header -->
+		<div class="contact-info span12">
+			<a href="/"><img src="/sites/default/files/images/PAMlogo.jpg" /></a>
+			<p>Manager of the Globersel Pactum Natural Resources Fund</p>
 		</div>
-	</header>
+	</div>
 </section>
 
 <section id="main-content">
 	<div class="container">
-	
 	  <header role="banner" id="page-header">
 	    <?php if ( $site_slogan ): ?>
 	      <p class="lead"><?php print $site_slogan; ?></p>
@@ -60,14 +108,11 @@
 	    <?php endif; ?>
 	
 	  </div>
-	</div>
-</section>
-<section id="footer">
 	  <footer class="footer container">
 	    <?php print render($page['footer']); ?>
 	  </footer>
+	</div>
 </section>
-
 
 	
 
